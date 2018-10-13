92ef0cafda912ef8817045c255f7186b5ab8a1f2
Stephen Malek <malek.stephen44@gmail.com>
2018-10-13 14:38:07 -0600

    add foods to meal completed

--- app.js
+++ app.js
@@ -71,29 +71,89 @@ app.delete('/api/v1/foods/:id', (request, response) => {
 })
 
 app.get('/api/v1/meals', (request, response) => {
-  database.from('meals')
-    .join('meal_foods', 'meals.id', '=', 'meal_foods.meal_id')
-    .join('foods', 'meal_foods.food_id', '=', 'foods.id')
+  database('meals')
+    .leftJoin('meal_foods', 'meals.id', '=', 'meal_foods.meal_id')
+    .leftJoin('foods', 'meal_foods.food_id', '=', 'foods.id')
     .select('meals.id', 'meals.name', 'meal_foods.food_id', 'foods.name as food_name', 'foods.calories')
     .orderBy('meals.id')
     .then((meals) => {
-      var formattedData = []
-      var meal_ids = []
-      for(var i=0; i < meals.length; i++){
-        if (meal_ids.includes(meals[i].id)){
-          formattedData[meal_ids.indexOf(meals[i].id)]
-            .foods.push({id: meals[i].food_id,
-                         name: meals[i].food_name,
-                         calories: meals[i].calories})
-        } else {
-          meal_ids.push(meals[i].id);
-          formattedData.push({ id: meals[i].id,
-                               name: meals[i].name,
-                               foods: [ {id: meals[i].food_id,
-                                         name: meals[i].food_name,
-                                         calories: meals[i].calories} ]})
-        }
-      }
-      response.status(200).json(formattedData)
-    })
+      response.status(200).json(formatMeals(meals)) })
+    .catch((error) => {
+      response.status(500).json({ error }) })
 })
+
+app.get('/api/v1/meals/:meal_id/foods', (request, response) => {
+  database('meals')
+    .leftJoin('meal_foods', 'meals.id', '=', 'meal_foods.meal_id')
+    .leftJoin('foods', 'meal_foods.food_id', '=', 'foods.id')
+    .where('meals.id', request.params.meal_id)
+    .select('meals.id', 'meals.name', 'meal_foods.food_id', 'foods.name as food_name', 'foods.calories')
+    .then((meals) => {
+      var status = 404;
+      var body = {status: 404, error: 'Not Found'}
+      if (meals.length !== 0){
+        status = 200
+        body = formatMeals(meals)[0]
+      }
+      response.status(status).json(body) })
+    .catch((error) => {
+      response.status(500).json({ error }) })
+})
+
+app.post('/api/v1/meals/:meal_id/foods/:id', (request, response) => {
+    var mealObj;
+    var foodObj;
+    database('meals').where('id', request.params.meal_id).first()
+      .then((meal) => {
+        mealObj = meal;
+        return database('foods').where('id', request.params.id).first()
+      })
+      .then((food) => {
+        foodObj = food;
+      })
+      .then(() => {
+        if (foodObj && mealObj){
+          return database('meal_foods').insert([{
+            meal_id: request.params.meal_id,
+            food_id: request.params.id
+          }], 'id')
+        }
+      })
+      .then((result) => {
+        if (result) {
+          response.status(200).json({message: `Successfully added ${foodObj.name} to ${mealObj.name}`})
+        } else {
+          response.status(404).json({status: 'Not Found - Parameters Missing'})
+        }
+      })
+      .catch((error) => {
+        response.status(500).json({ error })
+      })
+
+})
+
+function formatMeals(meals){
+  var formattedData = []
+  var meal_ids = []
+  for(var i=0; i < meals.length; i++){
+    var food = {id: meals[i].food_id,
+                name: meals[i].food_name,
+                calories: meals[i].calories}
+
+    if (meal_ids.includes(meals[i].id) && meals[i].food_id) {
+      formattedData[meal_ids.indexOf(meals[i].id)].foods.push(food)
+    } else if (meals[i].food_id) {
+      meal_ids.push(meals[i].id)
+      formattedData.push({ id: meals[i].id,
+                           name: meals[i].name,
+                           foods: [ food ]})
+    } else {
+      meal_ids.push(meals[i].id)
+      formattedData.push({ id: meals[i].id,
+                           name: meals[i].name,
+                           foods: []
+                         })
+    }
+  }
+  return formattedData;
+}
--- db/seeds/dev/03meal_foods.js
+++ db/seeds/dev/03meal_foods.js
@@ -3,12 +3,6 @@ exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
 
   return Promise.all([
-
-    // Insert a single paper, return the paper ID, insert 2 footnotes
-    knex('meal_foods').insert({
-      food_id: 2, meal_id: 1 }, 'id'),
-    knex('meal_foods').insert({
-      food_id: 3, meal_id: 1 }, 'id'),
     knex('meal_foods').insert({
       food_id: 1, meal_id: 2 }, 'id'),
     knex('meal_foods').insert({
@@ -20,7 +14,7 @@ exports.seed = function(knex, Promise) {
     knex('meal_foods').insert({
       food_id: 3, meal_id: 4 }, 'id'),
     knex('meal_foods').insert({
-      food_id: 5, meal_id: 4 }, 'id')
+      food_id: 2, meal_id: 4 }, 'id')
     .then(() => console.log('Seeding mealFoods complete!'))
     .catch(error => console.log(`Error seeding mealFoods data: ${error}`))
   ])
