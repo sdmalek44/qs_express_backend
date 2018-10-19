fc0b0f3949de506779d0b365ba6c8898686790e9
Stephen Malek <malek.stephen44@gmail.com>
2018-10-18 20:11:35 -0600

    add connection to knexfile

--- knexfile.js
+++ knexfile.js
@@ -16,11 +16,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'postgresql-tetrahedral-35190',
-      user:     'stephen',
-      password: '1234'
-    },
+    connection: 'postgres://iooqcvczjtnovd:11be7f3b90cfd7186d0e58123954dbfe482f6e8c063cb28afa78c3dc617335a3@ec2-54-221-225-11.compute-1.amazonaws.com:5432/ddhimtijdqip9n',
     migrations: {
       directory: './db/migrations'
     },
