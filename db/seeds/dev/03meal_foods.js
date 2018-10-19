9d61fe018db618a304c0fb1699ee58a5c7492d58
Stephen Malek <malek.stephen44@gmail.com>
2018-10-18 20:26:41 -0600

    update seed file to seed different dates

--- db/seeds/dev/03meal_foods.js
+++ db/seeds/dev/03meal_foods.js
@@ -1,18 +1,44 @@
 
 exports.seed = function(knex, Promise) {
+  var date = new Date(2018, 10, 16)
+  var date2 = new Date(2018, 10, 17)
+  var date3 = new Date(2018, 10, 18)
   return Promise.all([
     knex('meal_foods').insert({
-      food_id: 1, meal_id: 2 }, 'id'),
+      food_id: 1, meal_id: 2, created_at: date, updated_at: date }, 'id'),
     knex('meal_foods').insert({
-      food_id: 4, meal_id: 3 }, 'id'),
+      food_id: 4, meal_id: 3, created_at: date, updated_at: date }, 'id'),
     knex('meal_foods').insert({
-      food_id: 2, meal_id: 3 }, 'id'),
+      food_id: 2, meal_id: 3, created_at: date, updated_at: date }, 'id'),
     knex('meal_foods').insert({
-      food_id: 1, meal_id: 4 }, 'id'),
+      food_id: 1, meal_id: 4, created_at: date, updated_at: date }, 'id'),
     knex('meal_foods').insert({
-      food_id: 3, meal_id: 4 }, 'id'),
+      food_id: 3, meal_id: 4, created_at: date, updated_at: date }, 'id'),
     knex('meal_foods').insert({
-      food_id: 2, meal_id: 4 }, 'id')
-    .catch(error => console.log(`Error seeding mealFoods data: ${error}`))
+      food_id: 2, meal_id: 4, created_at: date, updated_at: date }, 'id'),
+    knex('meal_foods').insert({
+      food_id: 1, meal_id: 2, created_at: date2, updated_at: date2 }, 'id'),
+    knex('meal_foods').insert({
+      food_id: 4, meal_id: 3, created_at: date2, updated_at: date2 }, 'id'),
+    knex('meal_foods').insert({
+      food_id: 2, meal_id: 3, created_at: date2, updated_at: date2 }, 'id'),
+    knex('meal_foods').insert({
+      food_id: 1, meal_id: 4, created_at: date2, updated_at: date2 }, 'id'),
+    knex('meal_foods').insert({
+      food_id: 3, meal_id: 4, created_at: date2, updated_at: date2 }, 'id'),
+    knex('meal_foods').insert({
+      food_id: 2, meal_id: 4, created_at: date2, updated_at: date2 }, 'id'),
+    knex('meal_foods').insert({
+      food_id: 1, meal_id: 2, created_at: date3, updated_at: date3 }, 'id'),
+    knex('meal_foods').insert({
+      food_id: 4, meal_id: 3, created_at: date3, updated_at: date3 }, 'id'),
+    knex('meal_foods').insert({
+      food_id: 2, meal_id: 3, created_at: date3, updated_at: date3 }, 'id'),
+    knex('meal_foods').insert({
+      food_id: 1, meal_id: 4, created_at: date3, updated_at: date3 }, 'id'),
+    knex('meal_foods').insert({
+      food_id: 3, meal_id: 4, created_at: date3, updated_at: date3 }, 'id'),
+    knex('meal_foods').insert({
+      food_id: 2, meal_id: 4, created_at: date3, updated_at: date3 }, 'id')
   ])
 };
