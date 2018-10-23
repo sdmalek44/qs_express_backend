c19f483ace311b4db0228f6264de1dac7db6fee7
Stephen Malek <malek.stephen44@gmail.com>
2018-10-23 17:53:17 -0600

    add access controll allow origin

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ const Food = require('./lib/models/food.js')
 
 
 app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Quantified Self'
